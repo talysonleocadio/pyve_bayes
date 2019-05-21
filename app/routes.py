1a2be413d857c5877484332cf445ddf658771af6
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-05-21 18:53:51 -0300

    Modificação da rota para realizar as predições com as entradas do usuário'

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from app import app
+from utils.inputs_utils import parse_request_data, generate_dataset_from_input
+import bayes.classifier_nb as classifier
+from utils.file_utils import get_dataset_file_path
+import datasets.dataset as ds
+from configs.config_utils import get_configs
+from utils.dict_utils import extract_dict_fields
 
 
 @app.route('/')
@@ -10,5 +16,20 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict_from_user_data():
-    user_inputs = request.form.to_dict()
-    return(f'{user_inputs}')
+    parsed_request = parse_request_data(request.form.to_dict())
+    input_dataset = generate_dataset_from_input(parsed_request)
+
+    dataset_file_path = get_dataset_file_path()
+    dataset = ds.get_dataset_from_file(dataset_file_path)
+
+    loaded_json = get_configs()
+    model_properties = extract_dict_fields(loaded_json, ['model_properties'])
+    inputs, output = ds.extract_model_propeties(dataset, model_properties)
+
+    model = classifier.create_model()
+    model.fit(inputs, output.values.ravel())
+    prediction = model.predict(input_dataset)
+
+    return(render_template(
+        'prediction.html', title='Predict',
+        predict=prediction[0], bmi=parsed_request['bmi']))

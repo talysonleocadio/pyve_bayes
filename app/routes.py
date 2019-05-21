d874fdaacc2205a2392b975c7f69b198a9a9577d
Talyson Leocadio <talyson.leocadio@climas.waycarbon.com>
2019-05-21 13:48:52 -0300

    Rota criada para receber informações do form e realizar as predições

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 
 
@@ -6,3 +6,9 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_from_user_data():
+    user_inputs = request.form.to_dict()
+    return(f'{user_inputs}')

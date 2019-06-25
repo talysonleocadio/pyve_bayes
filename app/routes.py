0e9d140ca13523f8249d9c6459f27529fdb53e0e
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-06-24 23:29:53 -0300

    Metrics! Yeah baby

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from utils.dict_utils import extract_dict_fields
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html', title='Home')
 
@@ -33,3 +33,30 @@ def predict_from_user_data():
     return(render_template(
         'prediction.html', title='Predict',
         predict=prediction[0], bmi=parsed_request['bmi']))
+
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    dataset_file_path = get_dataset_file_path()
+    dataset = ds.get_dataset_from_file(dataset_file_path)
+
+    loaded_json = get_configs()
+    model_properties = extract_dict_fields(loaded_json, ['model_properties'])
+    inputs, output = ds.extract_model_propeties(dataset, model_properties)
+
+    model = classifier.create_model()
+    splitted_dataset = classifier.splitting_dataset(inputs, output)
+    model.fit(
+        splitted_dataset['fit']['inputs'],
+        splitted_dataset['fit']['outputs'])
+
+    prediction_tests = model.predict(splitted_dataset['test']['inputs'])
+    confusion_matix = classifier.get_confusion_matrix(
+        splitted_dataset['test']['outputs'],
+        prediction_tests)
+
+    metrics = classifier.get_metrics(confusion_matix)
+
+    return render_template('metrics.html',
+                           title='Metrics',
+                           view_metrics=metrics)
--- bayes/classifier_nb.py
+++ bayes/classifier_nb.py
@@ -1,4 +1,6 @@
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 
 def create_model():
@@ -12,4 +14,37 @@ def fit_model(data_dict, model):
 
 def predict_model(new_contract, model):
     return model.predict(new_contract)
+
+
+def splitting_dataset(dataset_inputs, dataset_outputs):
+    inputs_fit, inputs_test, outputs_fit, outputs_test = (
+        train_test_split(dataset_inputs,
+                         dataset_outputs,
+                         test_size=0.30)
+    )
+    return {
+        'fit': {
+            'inputs': inputs_fit,
+            'outputs': outputs_fit
+        },
+        'test': {
+            'inputs': inputs_test,
+            'outputs': outputs_test
+        }
+    }
+
+
+def get_confusion_matrix(test_outputs, predicted_test_outputs):
+    return confusion_matrix(test_outputs, predicted_test_outputs).ravel()
+
+
+def get_metrics(confusion_matrix):
+    TN, FP, FN, TP = confusion_matrix
+
+    return {
+        'accuracy': (TP + TN) / float(TP + TN + FP + FN),
+        'error': (FP + FN) / float(TP + TN + FP + FN),
+        'sensivity': TP / float(FN + TP),
+        'specificity': TN / float(TN + FP)
+    }
 
